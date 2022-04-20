3c5c084185e32042cfde00880da7e846a2093218
Gustavo Cortes <gustavocortes981@gmail.com>
2022-04-20 10:09:11 -0300

    Add files via upload

---
+++ JavaScript/Pegando dados do usuario/dados.js
@@ -0,0 +1,19 @@
+let people = prompt("Nome da pessoa mais velha")
+
+let age = prompt("Idade da pessoa mais velha")
+
+let youngerPerson = prompt("Nome da pessoa mais nova")
+
+let youngerAge = prompt("Idade da pessoa mais nova")
+
+let ageDifference = age - youngerAge
+
+// alert("Pessoa mais velha é "+people+ " com " +age+ " anos")
+
+// alert("Pessoa mais nova é "+youngerPerson+ " com " +youngerAge+ " anos")
+
+// alert("A diferença entre as idades é de: " +ageDifference+ " Anos")
+
+alert("Pessoa mais velha é "+people+ " com " +age+ " anos" +
+"\n\nPessoa mais nova é "+youngerPerson+ " com " +youngerAge+ " anos" +
+"\n\nA diferença entre as idades é de: " +ageDifference+ " Anos")
